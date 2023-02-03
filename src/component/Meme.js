a645152942e00760f992ae6c10e07fccf6735195
Collins Tatang <makungongcollins@gmail.com>
2023-02-03 17:23:29 +0100

    Add random imge function with useState

--- src/component/Meme.js
+++ src/component/Meme.js
@@ -1,13 +1,26 @@
+import React, { useState } from "react";
 import "./css/Meme.css";
+import memesData from "./memesData.js";
+
 
 const Meme= () => {
+    
+    const [memeImage, setMemeImage] = useState()
+    
+const getMemeImage = () => {
+     const memeArray = memesData.data.memes
+     const randomNumber = Math.floor(Math.random() * memeArray.length)
+     setMemeImage(memeArray[randomNumber].url) 
+}
+
     return(
         <main>
-            <form className="form">
+            <img src={memeImage} alt="No Meme Avialable"/>
+            <div className="form">
                 <input type="text" placeholder="Shut up" className="form--inputs" />
                 <input type="text" placeholder="and take my money" className="form--inputs"/>
-                <button className="form--btn">Get a new meme image  🖼</button>
-            </form>   
+                <button onClick={getMemeImage} className="form--btn">Get a new meme image  🖼</button>
+            </div>   
         </main>
     )
 }
---
+++ src/component/memesData.js
@@ -0,0 +1,24 @@
+const memesData = {
+    "success": true,
+    "data": {
+        "memes": [
+            {
+                "id": "181913649",
+                "name": "Drake Hotline Bling",
+                "url": "https://i.imhflip.com/30b1gx.jpg",
+                "width":1200,
+                "height":1200,
+                "box_count": 2
+            },
+            {
+                "id": "87743020",
+                "name": "Tow Buttons",
+                "url": "https://i.imhflip.com/1g8my4.jpg",
+                "width":600,
+                "height":108,
+                "box_count": 3
+            }
+        ]
+    }
+}
+export default memesData;
