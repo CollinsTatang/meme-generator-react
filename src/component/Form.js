1dd44ec4adb6882686c0b27816d468c1f334a26b
Collins Tatang <makungongcollins@gmail.com>
2023-03-08 09:44:26 +0100

    Add form state object

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,9 +1,33 @@
+import { useState } from "react";
+
 const Form = () => {
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: ""
+    });
+
+    function handleChange(event){
+       
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
     return (
         <form>
             <input
                 type="text"
                 placeholder="First Name"
+                onChange={handleChange}
+                name="firstName"
+            />
+            <input
+                type="text"
+                placeholder="Last Name"
+                onChange={handleChange}
+                name="lastName"
             />
         </form>
     )
