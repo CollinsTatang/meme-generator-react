9936b2c02d5dc0ac44f5005058c1220c23765f97
Collins Tatang <makungongcollins@gmail.com>
2023-03-08 06:27:58 +0100

    Add ternary operator to joke button

--- src/component/Joke.js
+++ src/component/Joke.js
@@ -13,7 +13,7 @@ const Joke = (props) => {
         <div>
             {props.setup && <h3>{props.setup}</h3>}
             {isShown && <p>{props.punchline}</p>}
-            <button onClick={toggleShown}>Show Punchline</button>
+            <button onClick={toggleShown}> {isShown ? "Hide" : "Show"} Punchline</button>
             <hr />
         </div>
     )
