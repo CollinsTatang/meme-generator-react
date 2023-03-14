570ddf9958c3187b552425bf64535e089ee6664d
Collins Tatang <makungongcollins@gmail.com>
2023-03-14 05:38:11 +0100

    Add API call that depend on count button

--- src/component/APIdata.js
+++ src/component/APIdata.js
@@ -2,16 +2,23 @@ import { useState, useEffect } from "react"
 
 const APIdata = () => {
     const [StarWarsData, setStarWarsData] = useState({})
+    const [count, setCount] = useState(1)
+
+    const addCourt = () => {
+        setCount((count) => count + 1)
+    }
 
         useEffect( () => {
-            fetch("https://swapi.dev/api/people/1")
+            fetch(`https://swapi.dev/api/people/${count}`)
                 .then(res => res.json())
                 .then(data => setStarWarsData(data))
-        }, [])
+        }, [count])
 
     return(
         <div>
+             <button onClick={addCourt}>Get Next</button>
             <pre>{JSON.stringify(StarWarsData, null, 2)}</pre>
+                <button onClick={addCourt}>Get Next API Call</button>
         </div>
     )
 }
